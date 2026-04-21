eed5c52001a269c190f0ba96e8effd663fe181a8
Blue640 <tell62001@mail.ru>
2026-04-21 13:05:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,25 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
 
 function createWindow() {
-    // Создаем окно рации
     const win = new BrowserWindow({
         width: 380,
         height: 720,
-        resizable: false, // Запрещаем менять размер для сохранения дизайна
-        autoHideMenuBar: true, // Убираем верхнее меню (Alt, File и т.д.)
-        icon: path.join(__dirname, 'icon.ico'), // Если сделаешь иконку
+        resizable: false,
+        autoHideMenuBar: true,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
         }
     });
 
-    // Загружаем твой HTML файл
     win.loadFile('index.html');
 
-    // ОБРАБОТЧИК ДЛЯ КНОПКИ PIN (ПОВЕРХ ВСЕХ ОКОН)
+    // Функция закрепления окна поверх остальных
     ipcMain.on('set-on-top', (event, mode) => {
-        console.log("Always on top mode:", mode);
         win.setAlwaysOnTop(mode);
     });
 }
 
-// Запуск приложения
 app.whenReady().then(() => {
     createWindow();
 
@@ -34,7 +28,6 @@ app.whenReady().then(() => {
     });
 });
 
-// Закрытие приложения
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit();
 });
